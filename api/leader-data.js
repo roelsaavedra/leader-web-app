be98ff84b4ada74b89fa57595bf759b8ee517dfb
roelsaavedra <roeladriannesaavedra@gmail.com>
2025-08-19 18:47:53 +0800

    Update leader-data.js

--- api/leader-data.js
+++ api/leader-data.js
@@ -2,17 +2,13 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST allowed' });
   }
-
   const { email } = req.body;
-
   try {
-    const scriptResponse = await fetch('https://script.google.com/macros/s/AKfycbwHEFqfhJTT0JeZCihn_PKXZQUmaw4fVBxtMTWknwXKeGOJ9D9yNufxaiConuc0jJFTGQ/exec', {
+    const scriptResponse = await fetch('https://script.google.com/macros/.../exec', {
       method: 'POST',
       body: new URLSearchParams({ email }),
     });
-
     const data = await scriptResponse.json();
-
     res.status(200).json(data);
   } catch (err) {
     console.error('Proxy error:', err);
