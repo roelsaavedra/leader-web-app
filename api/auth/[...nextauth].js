8563b72f0cda50c3a5c5ccadb67799bb397de342
roelsaavedra <roeladriannesaavedra@gmail.com>
2025-08-22 18:56:30 +0800

    Update [...nextauth].js

--- api/auth/[...nextauth].js
+++ api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google.js";
 
 const handler = NextAuth({
   providers: [
