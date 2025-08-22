3d28214a6a74f8b88414c1176d692a6311171cf5
roelsaavedra <roeladriannesaavedra@gmail.com>
2025-08-22 18:33:18 +0800

    Update [...nextauth].js

--- api/auth/[...nextauth].js
+++ api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { GoogleProvider } from "next-auth/providers";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -9,4 +9,7 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
